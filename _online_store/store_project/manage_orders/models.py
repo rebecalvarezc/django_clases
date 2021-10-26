464b619f0234a06e2e24e97415315ed379b37d97
rebecalvarezc <realce@alumni.uv.es>
2021-10-26 11:09:23 +0200

    created first tables using models

--- _online_store/store_project/manage_orders/models.py
+++ _online_store/store_project/manage_orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+# El código SQL lo crea Django por detrás.
+
+class Clients(models.Model):
+    name = models.CharField(max_length=30)  # de tipo texto.
+    address = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=13)
+
+
+class Goods(models.Model):
+    name = models.CharField(max_length=30)
+    section = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+
+class Orders(models.Model):
+    number = models.IntegerField()
+    date = models.DateField()
+    delivered = models.BooleanField()
