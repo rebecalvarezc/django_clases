fb9921acc8fb2ffd998f19995eaba576b8857b71
rebecalvarezc <realce@alumni.uv.es>
2021-10-27 11:50:53 +0200

    changed folders

--- _online_store/store_project/manage_orders/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from _online_store.store_project.manage_orders.models import Clients
-
-# Register your models here.
-admin.site.register(Clients)
---
+++ store_project_/manage_orders/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from manage_orders.models import Clients
+
+# Register your models here.
+admin.site.register(Clients)
