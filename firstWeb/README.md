eb80ac5f18c662376737a9d37032285bd0849ba5
rebecalvarezc <realce@alumni.uv.es>
2021-10-29 15:13:13 +0200

    minor changes in the home html file

---
+++ firstWeb/README.md
@@ -0,0 +1,5 @@
+## Lessons Learned 🏍
+
+Bootstrap es un Framework CSS creado por twitter en el 2011, este sirve para dar un formato a un sitio web usando librerías css.
+La principal característica de este framework es que es 'responsive' (a.k.a: se ve perfectamente en cualquier dispositivo).
+Otra ventaja es que es compatible con la mayoría de los navegadores, además de aportar rapidez al diseño web y permitir el uso de LESS.
--- firstWeb/webProject/views.py
+++ firstWeb/webProject/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'webProject/home.html')
 
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'webProject/services.html')
 
 
 def store(request):
-    return render(request, 'store.html')
+    return render(request, 'webProject/store.html')
 
 
 def contact_us(request):
-    return render(request, 'contact_us.html')
+    return render(request, 'webProject/contact_us.html')
 
 
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, 'webProject/blog.html')
