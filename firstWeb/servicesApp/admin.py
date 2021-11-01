2a8d055ec26e5d85b29eab8d15a3515b8e3a8766
rebecalvarezc <realce@alumni.uv.es>
2021-11-01 17:06:54 +0100

    created class to show two of the table parameters in services

--- firstWeb/servicesApp/admin.py
+++ firstWeb/servicesApp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Service
 
+
 # Register your models here.
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Service, ServiceAdmin)
