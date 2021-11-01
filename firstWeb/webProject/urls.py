770544c5e8a08a4a71271066d915586890150301
rebecalvarezc <realce@alumni.uv.es>
2021-11-01 17:23:31 +0100

    saved images in media folder and fixed django to show the images

--- firstWeb/webProject/urls.py
+++ firstWeb/webProject/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from webProject.views import home, services, store, contact_us, blog
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('us', contact_us, name='contact'),
     path('blog', blog, name='blog'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
