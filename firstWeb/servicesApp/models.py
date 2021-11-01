6461b15adb29b1ed0e67b91d221cba3ace642c32
rebecalvarezc <realce@alumni.uv.es>
2021-11-01 16:43:59 +0100

    added ORM in models.py of the new app

--- firstWeb/servicesApp/models.py
+++ firstWeb/servicesApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+# creamos un mapeo ORM
+
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=200)
+    picture = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+# cuando creamos un modelo podemos especificar características llamadas meta
+    class Meta:
+        verbose_name = 'service'
+        verbose_name_plural = 'services'
+
+    def __str__(self):
+        return self.title
