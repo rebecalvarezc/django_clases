34abce5d48c8ac6dc6a54277439b24be5dc5e09a
rebecalvarezc <realce@alumni.uv.es>
2021-11-02 22:22:05 +0100

    changes in admin to be able to visualice the new classes in the administration panel

--- firstWeb/blogApp/admin.py
+++ firstWeb/blogApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categories, Post
+
 
 # Register your models here.
+class CatAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Categories, CatAdmin)
+admin.site.register(Post, PostAdmin)
