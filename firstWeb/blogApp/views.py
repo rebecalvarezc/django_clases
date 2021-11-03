6314069c4c8ac9438c1c9e40efbb2d14ce2d3dcc
rebecalvarezc <realce@alumni.uv.es>
2021-11-03 09:36:34 +0100

    set the template and the views to show the posts on the webpage

--- firstWeb/blogApp/views.py
+++ firstWeb/blogApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def blog(request):
-    return render(request, 'C:/Users/Rebeca/PycharmProjects/djangoProject/firstWeb/blogApp/templates/blog/blog.html')
+    post = Post.objects.all()
+    return render(request, 'C:/Users/Rebeca/PycharmProjects/djangoProject/firstWeb/blogApp/templates/blog/blog.html',
+                  {'posts': post})
