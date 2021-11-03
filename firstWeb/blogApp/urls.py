439059ae997643142f2dbfbfaa87f3c0bc363274
rebecalvarezc <realce@alumni.uv.es>
2021-11-03 10:04:51 +0100

    filtered the posts by categories via url

--- firstWeb/blogApp/urls.py
+++ firstWeb/blogApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blog
+from .views import blog, category
 
 
 urlpatterns = [
     path('', blog, name='blog'),
+    path('category/<category_id>/', category, name='category')
 ]
--- firstWeb/blogApp/views.py
+++ firstWeb/blogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Categories
 
 
 # Create your views here.
@@ -7,3 +7,11 @@ def blog(request):
     post = Post.objects.all()
     return render(request, 'C:/Users/Rebeca/PycharmProjects/djangoProject/firstWeb/blogApp/templates/blog/blog.html',
                   {'posts': post})
+
+
+def category(request, category_id):
+    cat = Categories.objects.get(id=category_id)
+    posts = Post.objects.filter(categories=cat)
+    return render(request,
+                  'C:/Users/Rebeca/PycharmProjects/djangoProject/firstWeb/blogApp/templates/blog/categories.html',
+                  {'categories': cat, 'posts': posts})
