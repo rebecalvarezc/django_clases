c5ffff6a6f54d0e5bcc5f72326ab4c24e763e4e0
rebecalvarezc <realce@alumni.uv.es>
2021-11-04 10:23:46 +0100

    set form as table

--- firstWeb/contactApp/forms.py
+++ firstWeb/contactApp/forms.py
@@ -5,6 +5,6 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label='name', max_length=30, required=True)
     email = forms.CharField(label='email', max_length=50, required=True)
-    content = forms.CharField(label='content', max_length=350)
+    content = forms.CharField(label='content', widget=forms.Textarea)
 
 
