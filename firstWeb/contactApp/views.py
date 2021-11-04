70d855bb58efd60c4103a00f3a2ed138c4b4aa5b
rebecalvarezc <realce@alumni.uv.es>
2021-11-04 17:55:59 +0100

    bug found when using send button. Also, added conditional in views to sabe the form info

--- firstWeb/contactApp/views.py
+++ firstWeb/contactApp/views.py
@@ -4,5 +4,11 @@ from .forms import ContactForm
 
 # Create your views here.
 def contact_us(request):
-    contact_form = ContactForm
+    contact_form = ContactForm()
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            content = request.POST.get('content')
     return render(request, 'contactApp/contact_us.html', {'form': contact_form})
