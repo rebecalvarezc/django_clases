043fcf97eb2b6931cc8f49c6a3803520a150d333
rebecalvarezc <realce@alumni.uv.es>
2021-11-09 13:28:44 +0100

    fixed bug in blogApp

--- firstWeb/blogApp/views.py
+++ firstWeb/blogApp/views.py
@@ -5,15 +5,16 @@ from .models import Post, Categories
 # Create your views here.
 def blog(request):
     post = Post.objects.all()
+    cat = [i.categories.all()[0] for i in post]
+    cat = list(set(cat))
     return render(request, 'blog/blog.html',
-                  {'posts': post})
+                  {'posts': post, 'categories': cat})
 
 
 def category(request, category_id):
     cat = Categories.objects.get(id=category_id)
     posts = Post.objects.filter(categories=cat)
+    print(cat)
     return render(request,
                   'blog/categories.html',
                   {'categories': cat, 'posts': posts})
-
-# TODO: Arreglar las etiquetas de las categorías al fondo de la página, salen repetidas
--- firstWeb/contactApp/views.py
+++ firstWeb/contactApp/views.py
@@ -13,15 +13,16 @@ def contact_us(request):
             email = request.POST.get('email')
             content = request.POST.get('content')
 
-            e_mail = EmailMessage('App Django Message',
-                                  f'El usuario con nombre {name} y con el mail {email} escribe: \n{content}', '',
-                                  'lolaf@gmail.com', reply_to=None)
-            try:
-                e_mail.send()
-                return redirect('/us/?valid')
-            except:
-                return redirect('/us/?novalid')
-    return render(request, 'contactApp/contact_us.html', {'form': contact_form})
+            # e_mail = EmailMessage('App Django Message',
+            #                       f'El usuario con nombre {name} y con el mail {email} escribe: \n{content}', '',
+            #                       'lolaf@gmail.com', reply_to=None)
+            # try:
+            #     e_mail.send()
+            #     return redirect('/us/?valid')
+            #
+            # except:
+            #     return redirect('/us/?novalid')
+            return render(request, 'contactApp/contact_us.html', {'form': contact_form})
 
 # TODO: arreglar que cuando se envía el formulario sale un error de 'this field is required'.
 # TODO: arreglar que el formulario no se envía ( url no cambia a ?valid)
