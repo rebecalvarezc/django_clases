eccd36fd84363a1c09427069d12e5b53f7dca26c
rebecalvarezc <realce@alumni.uv.es>
2021-11-10 14:06:47 +0100

    added products and categories using admin and added the objects to views.py

--- firstWeb/storeApp/admin.py
+++ firstWeb/storeApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ProdCat, Product
+
 
 # Register your models here.
+class ProdCategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(ProdCat, ProdCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- firstWeb/storeApp/views.py
+++ firstWeb/storeApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 
 def store(request):
-    return render(request, 'store/store.html')
+    product = Product.objects.all()
+    return render(request, 'store/store.html', {'products': product})
