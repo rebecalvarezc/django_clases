bff10a78f8754e78acf9aad2aa2bf84c2edc7173
rebecalvarezc <realce@alumni.uv.es>
2021-11-12 17:45:54 +0100

    fixed bug

--- firstWeb/cartApp/context_processor.py
+++ firstWeb/cartApp/context_processor.py
@@ -2,8 +2,7 @@ def total_cart_value(request):
     total = 0
     # if request.user.is_authenticated:
     for key, value in request.session['cart'].items():
-        total += float(value['price'] * value['quantity'])
-        # error aquí
+        total += float(value['price'])
     return {'total_cart_value': total}
 
 # like creating a global variable
