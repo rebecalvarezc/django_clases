2ffd1d1e69bd5dc4c3411717ea88ca6d4ec194d5
rebecalvarezc <realce@alumni.uv.es>
2021-11-12 16:14:26 +0100

    added widget template and first functionality and created urls file in cart

--- firstWeb/firstWeb/settings.py
+++ firstWeb/firstWeb/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processor.total_cart_value'
+                'cartApp.context_processor.total_cart_value'
             ],
         },
     },
